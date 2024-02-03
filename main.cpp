f7cad35e28b3468d951e9f00c381a185da4f7544
lillymarletta <marlettl@union.edu>
2024-02-03 00:39:45 +0000

    FINAL

--- main.cpp
+++ main.cpp
@@ -3,32 +3,27 @@
 #include "Ignition.h"
 #include "mbed.h"
 #include "arm_book_lib.h"
-#include "entryway_light.h"
+#include "headlight.h"
 #include "light_sensor.h"
 
 //=====[Defines]===============================================================
 
 #define DELAY_TIME  10
 
-//=====[Declaration and initialization of public global objects]===============
-
-//=====[Declaration and initialization of public global variables]=============
-
-//=====[Declarations (prototypes) of public functions]=========================
-
 //=====[Main function, the program entry point after power on or reset]========
 
 int main()
+//runs the functions from the other modules to implement the system
 {
     ignitionInit();
-    entryLightInit();
+    headlightInit();
     while (true) {
         updateIgnition();
         if (returnEngineStatus()){
-            entryLightUpdate();
+            headlightUpdate();
         }
         else{
-            entryLightInit();
+            headlightInit();
         }
         delay(DELAY_TIME);
     }
